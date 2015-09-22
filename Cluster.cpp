c806d693e8409b7d3c5e10ffc94abecf12bc22f0
dragon5hoarder <dragon5hoarder@yahoo.com>
2015-09-21 23:48:05 -0500

    final

---
+++ Cluster.cpp
@@ -0,0 +1,254 @@
+//
+// Created by dragon5hoarder on 9/19/2015.
+//
+
+#include "Cluster.h"
+#include <cassert>
+
+namespace Clustering {
+
+    Cluster::Cluster() {
+        size = 0;
+        points = nullptr;
+    }
+
+    Cluster::Cluster(const Cluster &copyCluster) {
+        size = copyCluster.size;
+        points = new Node;
+        NodePtr tester = points;
+        NodePtr copyTester = copyCluster.points;
+        for (int i = 0; i < size; i++){
+            tester->point = copyTester->point;
+            tester->next = new Node;
+            tester = tester->next;
+            copyTester = copyTester->next;
+        }
+        tester->next = nullptr;
+    }
+
+    void Cluster::operator=(const Cluster &copyCluster){
+        if (this == &copyCluster) {
+            return;
+        }
+        size = copyCluster.size;
+        points = new Node;
+        NodePtr tester = points;
+        NodePtr copyTester = copyCluster.points;
+        for (int i = 0; i < size; i++){
+            tester->point = copyTester->point;
+            tester->next = new Node;
+            tester = tester->next;
+            copyTester = copyTester->next;
+        }
+        tester->next = nullptr;
+    }
+
+    Cluster::~Cluster(){
+        delete points;
+    }
+
+    // TODO Does not put Points in lexographic order
+    void Cluster::add(const PointPtr &addedPt) {
+        NodePtr tester;
+        NodePtr placeHolder = new Node;
+        placeHolder->point = addedPt;
+        bool statement = true;
+        if (points == nullptr || *(points->point) <= *(placeHolder->point))
+        {
+            placeHolder->next = points;
+            points = placeHolder;
+            size++;
+        }
+
+        else
+        {
+
+            tester = points;
+            while (tester->next!= nullptr && *(tester->next->point) < *(placeHolder->point))
+            {
+                tester = tester->next;
+            }
+            placeHolder->next = tester->next;
+            tester->next = placeHolder;
+            size++;
+        }
+
+
+
+    }
+
+    const PointPtr& Cluster::remove(const PointPtr &deletedPt) {
+        NodePtr tester;
+        NodePtr deleted;
+        bool statement = true;
+        //assert (size == 0);
+
+        tester = points;
+        if (*(points->point) == *deletedPt) {
+            deleted = tester;
+            points = tester->next;
+            delete deleted;
+            size--;
+            return deletedPt;
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (*(tester->next->point) == *deletedPt) {
+                deleted = tester->next;
+                if (tester->next->next == nullptr) {
+                    tester->next = nullptr;
+                }
+                else {
+                    tester->next = tester->next->next;
+                }
+                delete deleted;
+                size--;
+                return deletedPt;
+
+            }
+            tester = tester->next;
+        }
+    }
+
+    bool operator==(const Cluster &lhs, const Cluster &rhs){
+        NodePtr testerLhs = lhs.points;
+        NodePtr testerRhs = rhs.points;
+        if (lhs.size == rhs.size) {
+            for (int i = 0; i < lhs.size; i++) {
+                if (testerLhs->point != testerRhs->point) {
+                    testerRhs = testerRhs->next;
+                    testerLhs = testerLhs->next;
+                    return false;
+                }
+
+            }
+            return true;
+        }
+        return false;
+    }
+
+
+
+    Cluster& Cluster::operator+=(Point &rhs){
+        add(&rhs);// TODO Will not compile with the const in parameter
+    }
+
+    Cluster& Cluster::operator-=(Point &rhs){
+        remove(&rhs);// TODO Will not compile with the const in parameter
+    }
+
+    Cluster& Cluster::operator+=(const Cluster &rhs){
+        NodePtr testerLhs = points;
+        NodePtr testerRhs = rhs.points;
+        int i = 0;
+        int j = 0;
+        do{
+            do {
+
+                if (testerLhs->point == testerRhs->point) { //TODO segmentation fault
+                    add(testerRhs->point);
+                    testerRhs = testerRhs->next;
+                    i++;
+                    testerLhs = testerLhs->next;
+                    j++;
+                }
+                else {
+                    testerRhs = testerRhs->next;
+                    i++;
+                }
+            }while (j < rhs.size);
+            testerLhs = testerLhs->next;
+            testerRhs = rhs.points;
+            i = 0;
+            j++;
+        }while(i < size);
+    }
+
+    Cluster& Cluster::operator-=(const Cluster &rhs){
+        NodePtr testerLhs = points;
+        NodePtr testerRhs = rhs.points;
+        int i = 0;
+        int j = 0;
+        do{
+            do {
+
+                if (testerLhs->point == testerRhs->point) {
+                    remove(testerRhs->point);
+                    testerRhs = testerRhs->next;
+                    i++;
+                    testerLhs = testerLhs->next;
+                    j++;
+                }
+                else {
+                    testerRhs = testerRhs->next;
+                    i++;
+                }
+            }while (j < rhs.size);
+            testerLhs = testerLhs->next;
+            testerRhs = rhs.points;
+            i = 0;
+            j++;
+        }while(i < size);
+    }
+
+    const Cluster operator+(const Cluster &lhs, const PointPtr &rhs){
+        Cluster tempCluster = lhs;
+        tempCluster.add(rhs);
+        return tempCluster;
+
+    }
+
+    const Cluster operator-(const Cluster &lhs, const PointPtr &rhs){
+        Cluster tempCluster = lhs;
+        tempCluster.remove(rhs);
+        return tempCluster;
+
+    }
+
+    const Cluster operator+(const Cluster &lhs, const Cluster &rhs){
+        Cluster tempCluster = lhs;
+        tempCluster += rhs;
+    }
+
+    const Cluster operator-(const Cluster &lhs, const Cluster &rhs){
+        Cluster tempCluster = lhs;
+        tempCluster -= rhs;
+    }
+
+
+
+
+
+    std::ostream &operator<<(std::ostream &os, const Cluster &output) {
+        NodePtr tester;
+        tester = output.points;
+        os << "Points: " << std::endl;
+        for (int i = 0; i < output.size; i++) {
+            os << *(tester->point);
+            tester = tester->next;
+
+        }
+        std::cout << std::endl;
+
+        return os;
+    }
+
+    std::istream &operator>>(std::istream &os, const Cluster &input) {
+        NodePtr tester;
+        tester = input.points;
+        std::cout << "Input Points: " << std::endl;
+        for (int i = 0; i < input.size; i++) {
+            os >> *(tester->point);
+            tester = tester->next;
+
+        }
+        std::cout << std::endl;
+
+        return os;
+    }
+
+
+}
+
+
+
---
+++ Cluster.h
@@ -0,0 +1,59 @@
+//
+// Created by dragon5hoarder on 9/19/2015.
+//
+
+#ifndef INT_PA2_CLUSTER_H
+#define INT_PA2_CLUSTER_H
+
+#include "Point.h"
+
+namespace Clustering {
+    typedef Point *PointPtr;
+    typedef struct Node *NodePtr;
+
+    struct Node{
+        PointPtr point;
+        Node *next;
+    };
+
+    class Cluster{
+
+    private:
+        NodePtr points;
+        int size;
+
+    public:
+        Cluster();
+        Cluster(const Cluster &copyCluster);
+        void operator=(const Cluster &copyCluster);
+        ~Cluster();
+
+        // Set functions
+        void add(const PointPtr &addedPt);
+        const PointPtr &remove(const PointPtr &deletedPt);
+
+        // overloaded operators
+        friend bool operator==(const Cluster &lhs, const Cluster &rhs);
+
+        Cluster &operator+=(Point &rhs); // add point
+        Cluster &operator-=(Point &rhs); // remove point
+
+        Cluster &operator+=(const Cluster &rhs); // union
+        Cluster &operator-=(const Cluster &rhs); // difference
+
+        friend const Cluster operator+(const Cluster &lhs, const PointPtr &rhs);
+        friend const Cluster operator-(const Cluster &lhs, const PointPtr &rhs);
+
+        friend const Cluster operator+(const Cluster &lhs, const Cluster &rhs);
+        friend const Cluster operator-(const Cluster &lhs, const Cluster &rhs);
+
+
+
+
+        friend std::ostream &operator<<(std::ostream &os, const Cluster &output);
+        friend std::istream &operator>>(std::istream &os, const Cluster &input);
+    };
+
+}
+
+#endif //INT_PA2_CLUSTER_H
